ee509367c251c2321adc7f625a23d245bf16b304
snachited <snachited@gmail.com>
2024-05-18 12:41:54 -0400

    Initial commit

---
+++ biblio-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+export default function Home() {
+    return (
+        <main className={styles.main}>
+        <div className={styles.welcome}>
+            Soyez la bienvenue sur biblio-app
+        </div>
+        </main>
+    );
+}
